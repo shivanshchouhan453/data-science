b78f1f0e269c486b8ed5638d17131a253c5d75aa
Shivansh chouhan <134367105+shivanshchouhan453@users.noreply.github.com>
2024-02-21 12:31:00 +0530

    plot_with marker

---
+++ plot_ linedigram_marker.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+ypoints = np.array([2,6,4,6,2])
+#here x points values compiler convert in default value
+plt.plot(ypoints,marker='o')
+plt.show()
